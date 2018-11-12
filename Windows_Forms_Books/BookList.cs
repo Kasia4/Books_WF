ab047fbfdc133a12d45eea8f46b74ebaf9762e54
Kasia4 <katarzyna.rybacka4@gmail.com>
2018-11-12 19:49:13 +0100

    Move List to seperate file

---
+++ Windows_Forms_Books/BookList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Forms_Books
+{
+    public class BookList : IEnumerable<Book>
+    {
+        private List<Book> bookList;
+
+        public delegate void BookEventHandler(object sender, EventArgs args);
+        public event BookEventHandler RaiseAddEvent;
+        public event BookEventHandler RaiseDeleteEvent;
+        public event BookEventHandler RaiseEditEvent;
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return bookList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return bookList.GetEnumerator();
+        }
+    }
+}
+
